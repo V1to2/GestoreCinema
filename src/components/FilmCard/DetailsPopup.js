dc5f43a0c565124842493bd545ae5ad1d65d99d5
V1to2 <67048779+V1to2@users.noreply.github.com>
2022-05-08 22:02:09 +0200

    PopUp + add

--- src/components/FilmCard/DetailsPopup.js
+++ src/components/FilmCard/DetailsPopup.js
@@ -15,22 +15,47 @@ function DetailsPopup(props){
     useEffect(()=>{
         axios
         .get(
-            "https://"
+            "https://87.250.73.22/html/Ardizio/informatica/php/Progetto%20Cinema/api%20php/getProiezioniFilm.php?searchFilm_title=" + props.title
         )
-        .then(
-            //code ...
-        )
+        .then((result) => {
+            console.log(result.data)
+            
+        })
     },[]);
 
+    const stile = {
+        backgroundColor: "white",
+        display: "flex",
+        justifyContent: "space-between"
+    }
+
+    const stilebottone = {
+        position: "absolute",
+        width: "2.6rem",
+        height: "2.6rem",
+        top: "-1.3rem",
+        right: "-1.3rem",
+    }
+
+    const stileimmagine = {
+        height: "100%",
+        width: 'auto',
+        paddingRight: '6%',
+    }
+
+    const stileTesto = {
+        width: "30rem"
+    }
+
     return(
-        <div className="PopupFilm-root">
-            <div onClick={props.close}>
-                <img src="https://www.ilsicomoro.it/js/skins/default/lightbox-close.png" alt="close" />
+        <div className="PopupFilm-root" style={stile}>
+            <div onClick={props.close} style={stilebottone}>
+                <img src="https://www.ilsicomoro.it/js/skins/default/lightbox-close.png" alt="close" draggable={ false } />
             </div>
             <div>
-                <img className="PopupFilm-image" src={props.imgPath} alt={props.originalTitle}/>
+                <img className="PopupFilm-image" style={stileimmagine} src={props.imgPath} alt={props.originalTitle}/>
             </div>
-            <div className="PopupFilm-info">
+            <div className="PopupFilm-info" style={stileTesto}>
                 <h1> {props.title} </h1>
                 <p>
                     {props.trama}
@@ -38,9 +63,9 @@ function DetailsPopup(props){
                 <pre>
                     Data Di Uscita:      {props.uscita}
                 </pre>
-            </div>
-            <div className="PopupFilm-Proiezioni">
-                { listaSaleProiezioni == null ? <div className="alert Warning"> you can't find this film in any Cinema</div> : listaSaleProiezioni.map((cine, i)=>{<Lista key={"cinema-" + cine + i} nomeCinema={cine} listaSale={cine} />}) }
+                <div className="PopupFilm-Proiezioni">
+                    { listaSaleProiezioni == null ? <div className="alert Warning"> We can't find this Film in any Cinema</div> : listaSaleProiezioni.map((cine, i)=>{<Lista key={"cinema-" + cine + i} nomeCinema={cine} listaSale={cine} />}) }
+                </div>
             </div>
         </div>
     );
