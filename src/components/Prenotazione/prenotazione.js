fe3d924d2432f513df9de67788571f5e45261531
V1to2 <67048779+V1to2@users.noreply.github.com>
2022-05-15 22:31:11 +0200

    Niente di che

---
+++ src/components/Prenotazione/prenotazione.js
@@ -0,0 +1,15 @@
+import React from "react";
+import './style.css'
+
+export default function About() {
+    return (
+        <>
+            <div className="main">
+                <div className="top">
+                    <p>asd</p>
+                </div>
+            </div>
+
+        </>
+    );
+}
